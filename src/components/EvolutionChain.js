a7c73ec62e5340547dc592321f0f43cd5e7d5e25
Casey The Great <casey@caseypitman.com>
2020-10-30 17:00:02 -0500

    Added EvolutionChain component

---
+++ src/components/EvolutionChain.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EvolutionChain = () => {
+  return (
+    <div>
+      <ul>
+        <li>evolution 1</li>
+        <li>evolution 2</li>
+        <li>evolution 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default EvolutionChain;
--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import placeholder from "../img/placeholder-image.png";
 import Button from "./Button";
+import EvolutionChain from "./EvolutionChain";
 
 const PokemonDetail = () => {
   return (
@@ -8,15 +9,16 @@ const PokemonDetail = () => {
       <div>
         <Button type='prev' />
       </div>
-      <img src={placeholder} alt='placeholder pic' style={{ width: "200px" }} />
-      <h2>number Name</h2>
-      <h3>type</h3>
-      {/* Evolution chain may be seperate module */}
-      <ul>
-        <li>evolution 1</li>
-        <li>evolution 2</li>
-        <li>evolution 3</li>
-      </ul>
+      <div>
+        <img
+          src={placeholder}
+          alt='placeholder pic'
+          style={{ width: "200px" }}
+        />
+        <h2>number Name</h2>
+        <h3>type</h3>
+        <EvolutionChain />
+      </div>
       <div>
         <Button type='next' />
       </div>
