788d56f98ac6b80c9196aee07a7dc260f73415f9
Casey The Great <casey@caseypitman.com>
2020-10-30 15:06:14 -0500

    Added PokeonList component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,11 @@
 import React from "react";
+import PokemonList from "./PokemonList";
 
 const Display = () => {
   return (
     <div>
       <h1>Display</h1>
+      <PokemonList />
     </div>
   );
 };
---
+++ src/components/PokemonList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const PokemonList = () => {
+  return (
+    <div>
+      <h1>List of Pokemon of selected type.</h1>
+      {/* <img src = {} alt = "pokemon art"/> */}
+      <h4>Pokemon number & name</h4>
+    </div>
+  );
+};
+
+export default PokemonList;
