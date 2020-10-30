39293a6aa3189546557405a40b79b76239f69d7e
Casey The Great <casey@caseypitman.com>
2020-10-30 14:29:31 -0500

    Added Footer component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Display from "./Display";
+import Footer from './Footer'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <hr></hr>
+      <Display />
+      <hr></hr>
+      <Footer />
     </div>
   );
 };
