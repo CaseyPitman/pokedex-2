81b0dc52b5e4da37dba6a6a83cae33385d48b17f
Casey The Great <casey@caseypitman.com>
2020-10-30 16:39:02 -0500

    Added pokeball img and copyright info to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import pokeball from "../img/pokeball.png"
 
 const Footer = () => {
   return (
     <div>
       <h1>Footer</h1>
+      <img src = {pokeball} style = {{'max-width': '100px'}}/>
+      <h3>&copy; 2020 | Casey Pitman </h3>
     </div>
   );
 };
