3ff7d5bd596b3378a4d2c52fb063f5805c922bca
Casey The Great <casey@caseypitman.com>
2020-10-30 14:20:00 -0500

    Added main Display component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from './Header'
-
+import Header from "./Header";
+import Display from "./Display";
 
 const App = () => {
   return (
     <div>
-      <Header/>
+      <Header />
     </div>
   );
 };
