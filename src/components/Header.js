46d78f879e4591f5547f5f4d474904bdad9ac19d
Casey The Great <casey@caseypitman.com>
2020-10-30 13:49:19 -0500

    Feature: Added Search component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Search from './Search'
 
 const Header = () => {
   return (
     <div>
       <h1>Header</h1>
+      <Search/>
     </div>
   );
 };
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+       <label>Search for Pokemon.</label>
+      <input type='text'></input>
+    </div>
+  );
+};
+
+export default Search;
