04e73fad1a4efcb13daaac91dcf42b16f0dfca80
Casey The Great <casey@caseypitman.com>
2020-10-30 15:11:07 -0500

    Added header image.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Search from "./Search";
 import FilterType from "./FilterType";
+import headerImg from "../img/starter-pokemon.jpg"
+
 
 const Header = () => {
   return (
     <div>
       <h1>Header</h1>
+      <img src = {headerImg} style = {{'max-width': '200px'}}/>
       <FilterType />
       <Search />
     </div>
