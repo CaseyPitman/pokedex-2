5390bc483c7e2c953a54137402db536812fbafca
Casey The Great <casey@caseypitman.com>
2020-10-31 11:58:31 -0500

    Added growlithe and squirtle images to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,28 @@
 import React from "react";
-import pokeball from "../img/pokeball.png"
+
+// Assets
+import growlithe from "../img/growlithe.jpg";
+import squirtleGlasses from "../img/squirtle-glasses.png"
+
+// Styles
+import footer from "../CSS/footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-      <img src = {pokeball} style = {{'max-width': '100px'}}/>
-      <h3>&copy; 2020 | Casey Pitman </h3>
+    <div className='footer'>
+      <img
+        className='footer-img'
+        src={growlithe}
+        style={{ "max-width": "100px" }}
+      />
+      <img
+        className='footer-img'
+        src={squirtleGlasses}
+        style={{ "max-width": "100px" }}
+      />
+      <div className='footer-text'>
+        <h3 className='copyright-text'>&copy; 2020 | Casey Pitman </h3>
+      </div>
     </div>
   );
 };
