84f480e2bff72081dadfb368392551889f2a75c1
Casey The Great <casey@caseypitman.com>
2020-10-31 12:10:03 -0500

    Used flexbox to space elements in Footer component.

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <div>
       <Header />
       <Display />
-      <hr></hr>
+  
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // Assets
 import growlithe from "../img/growlithe.jpg";
-import squirtleGlasses from "../img/squirtle-glasses.png"
+import squirtleGlasses from "../img/squirtle-glasses.png";
 
 // Styles
 import footer from "../CSS/footer.css";
@@ -10,19 +10,26 @@ import footer from "../CSS/footer.css";
 const Footer = () => {
   return (
     <div className='footer'>
-      <img
-        className='footer-img'
-        src={growlithe}
-        style={{ "max-width": "100px" }}
-      />
-      <img
-        className='footer-img'
-        src={squirtleGlasses}
-        style={{ "max-width": "100px" }}
-      />
-      <div className='footer-text'>
-        <h3 className='copyright-text'>&copy; 2020 | Casey Pitman </h3>
-      </div>
+
+        <div className='footer-img'>
+          <img
+            className='growlithe-footer-img'
+            src={growlithe}
+            alt='Growlithe leaping into the fray.'
+          />
+        </div>
+        <div className='footer-text'>
+          <h2>For Charlie</h2>
+          <h3 className='copyright-text'>&copy; 2020 | Casey Pitman </h3>
+        </div>
+        <div className='footer-img'>
+          <img
+            className='squirtle-footer-img'
+            src={squirtleGlasses}
+            alt='Squirtle looking cool in his shades.'
+          />
+        </div>
+  
     </div>
   );
 };
