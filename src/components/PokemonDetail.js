e4d2f03c34921c48041103bc6813c326d2ef1a63
Casey The Great <casey@caseypitman.com>
2020-11-01 08:30:05 -0600

    Added container for pokemon detail content

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,23 +1,32 @@
 import React from "react";
+
+// Assets
 import placeholder from "../img/placeholder-image.png";
+
+// Components
 import Button from "./Button";
 import EvolutionChain from "./EvolutionChain";
 
+// Styles
+import pokemonDetail from "../CSS/pokemonDetail.css";
+
 const PokemonDetail = () => {
   return (
-    <div>
-      <Button type='left' />
-      <div>
-        <img
-          src={placeholder}
-          alt='placeholder pic'
-          style={{ width: "200px" }}
-        />
-        <h2>number Name</h2>
-        <h3>type</h3>
-        <EvolutionChain />
+    <div className='pokemon-detail'>
+      <div className='pokemon-detail-container'>
+        <Button type='left' />
+        <div>
+          <img
+            src={placeholder}
+            alt='placeholder pic'
+            style={{ width: "200px" }}
+          />
+          <h2>number Name</h2>
+          <h3>type</h3>
+          <EvolutionChain />
+        </div>
+        <Button type='right' />
       </div>
-      <Button type='right' />
     </div>
   );
 };
