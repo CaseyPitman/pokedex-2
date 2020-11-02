550010e20a8ecd486210b5655478c12a999a76a6
Casey The Great <casey@caseypitman.com>
2020-11-02 13:37:08 -0600

    Set grid to 5 columns.

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -34,6 +34,102 @@ const PokemonList = () => {
         <h4 className = 'pokemon-list-item-number'>7</h4>
         <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
       </div>
+      
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+      
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+      
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+      
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+      
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
+
+      <div className = 'pokemon-list-item'>
+        <img className = 'pokemon-list-item-img'src={placeholder} alt='pokemon art' />
+        <h4 className = 'pokemon-list-item-number'>7</h4>
+        <h4 className = 'pokemon-list-item-name'>Squirtle</h4>
+      </div>
   
 
 
