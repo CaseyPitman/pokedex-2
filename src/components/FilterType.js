e12b6fa01d423902217597b3b3076d9e4cb5305b
Casey The Great <casey@caseypitman.com>
2020-11-03 13:50:49 -0600

    Set up api call for types for FilterType component.

--- src/components/FilterType.js
+++ src/components/FilterType.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// styles
 // Styles
-
 import header from "../CSS/header.css";
 
 const FilterType = () => {
+  useEffect(() => {
+    const types = async () => {
+      const response = await axios.get("https://pokeapi.co/api/v2/type");
+
+      console.log(response);
+    };
+    types();
+    // set the state
+  }, []);
+
   return (
     <div className='filter-type'>
       <select className='input-field'>
