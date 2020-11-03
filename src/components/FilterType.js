66bca9110310e929a02563420c8dadcececaaf48
Casey The Great <casey@caseypitman.com>
2020-11-03 15:28:14 -0600

    FilterType: mapped types to set options on select.

--- src/components/FilterType.js
+++ src/components/FilterType.js
@@ -5,24 +5,25 @@ import axios from "axios";
 import header from "../CSS/header.css";
 
 const FilterType = () => {
-const [types, setTypes] = useState([]);
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
     const getTypes = async () => {
       const response = await axios.get("https://pokeapi.co/api/v2/type");
 
-      console.log(response.data.results);
+      // console.log(response.data.results);
       setTypes(response.data.results);
     };
     getTypes();
   }, []);
 
-  
+  const typeOptions = types.map(({name}) => <option key={name}>{name}</option>);
 
   return (
     <div className='filter-type'>
       <select className='input-field'>
         <option>Sort by Type</option>
+        {typeOptions}
       </select>
     </div>
   );
