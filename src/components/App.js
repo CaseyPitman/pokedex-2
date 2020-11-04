b2553c0fd5b98bf741298e3c572034c5f9275b98
Casey The Great <casey@caseypitman.com>
2020-11-04 14:21:41 -0600

    Made Search component controlled.

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,8 @@ const App = () => {
     }
   }, [listUrl]);
 
+  
+
 
 
   const changeListType = url => {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 const Search = () => {
+
+ const [term, setTerm] = useState('');
+
+
+
+
   return (
-    <div className = 'search-input'>
-      <input className = 'input-field' type='text' placeholder = 'Search for Pokémon'></input>
+    <div className='search-input'>
+      <input
+        className='input-field'
+        type='text'
+        value = {term}
+        onChange = {(event) => setTerm(event.target.value)}
+        placeholder='Search for Pokémon'></input>
     </div>
   );
 };
