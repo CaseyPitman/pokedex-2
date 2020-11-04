d20b245996c144c4165fa14e2d580dae07ab76d8
Casey The Great <casey@caseypitman.com>
2020-11-04 09:55:07 -0600

    App: made call to retrieve data re: list of pokemon in the selected type.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
 
 // Components
 import Header from "./Header";
@@ -6,11 +8,25 @@ import Display from "./Display";
 import Footer from "./Footer";
 
 const App = () => {
-  const [listType, setListType] = useState("");
-  const [pokemonList, setPokemonList] = useState([]);
+  const [listUrl, setListUrl] = useState("");
+  const [pokemonListByType, setPokemonListByType] = useState([]);
+
+  // Call for list of pokemon in the requested type.
+  useEffect(() => {
+    if (listUrl !== "") {
+      const getPokemon = async () => {
+        console.log("test", listUrl);
+        const response = await axios.get( listUrl );
+
+        console.log(response.data.pokemon);
+        setPokemonListByType(response);
+      };
+      getPokemon();
+    }
+  }, [listUrl]);
 
   const changeListType = url => {
-    setListType(url);
+    setListUrl(url);
   };
 
   return (
