84e57ea74c7df19e3a316ce4c25f642929c47c26
Casey The Great <casey@caseypitman.com>
2020-11-05 14:44:18 -0600

    PokemonListItem: added state hook for pokemon type(s)

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,46 +1,41 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 // Dependencies
-import axios from 'axios';
+import axios from "axios";
 
 // Assets
-import placeholder from "../img/placeholder-image.png";
 import pokeball from "../img/pokeball.png";
 
+const PokemonListItem = props => {
+  const [img, setImg] = useState(pokeball);
+  const [name, setName] = useState("Pokemon");
+  const [number, setNumber] = useState(" ");
+  const [type, setType] = useState("Normal");
 
-const PokemonListItem = (props) => {
-const [img, setImg] = useState(pokeball);
-const [name, setName] = useState('Pokemon');
-const [number, setNumber] = useState(" ");
-
-
+  // Calls for Pokemon image, number, and name to display on list.
   useEffect(() => {
     const getPokemon = async () => {
-      const {data} = await axios.get(props.detailsUrl);
+      const { data } = await axios.get(props.detailsUrl);
 
       setNumber(data.id);
       setName(data.name);
-      setImg(data.sprites.front_default)
+      setImg(data.sprites.front_default);
       // console.log(data.sprites.other.official-artwork.front_default);
     };
     getPokemon();
-  }, []);
-
-
-
+  }, [props.detailsUrl]);
 
   return (
     <div className='pokemon-list-item'>
-      <img
-        className='pokemon-list-item-img'
-        src={img}
-        alt='pokemon art'
-      />
+      <img className='pokemon-list-item-img' src={img} alt='pokemon art' />
       <h4 className='pokemon-list-item-number'>{number}</h4>
-      <h4 className='pokemon-list-item-name'>{name}</h4>
+      {/* Capitalize Pokemon name */}
+      <h4 className='pokemon-list-item-name'>
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </h4>
+      <h4 className='pokemon-list-item-type'>Normal Flying</h4>
     </div>
   );
 };
-
 
 export default PokemonListItem;
