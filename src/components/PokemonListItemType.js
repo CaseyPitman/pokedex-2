142d08b83e5492b393c32cafd95a4a8c4c1077d9
Casey The Great <casey@caseypitman.com>
2020-11-05 14:53:00 -0600

    Added jsx PokemonListItemType component.

---
+++ src/components/PokemonListItemType.js
@@ -0,0 +1,15 @@
+const { useState } = require("react");
+
+import React, { useState, useEffect } from "react";
+
+
+
+// Should I make this an unorded list instead? 
+
+const PokemonListItemType = props => {
+  return (
+    <div className = "pokemon-list-item-type">
+      <h4 className='pokemon-list-item-type-text'>Normal Flying</h4>
+    </div>
+  );
+};
