f68fc8387848ba07595beaaeae1759c9962457ce
Casey The Great <casey@caseypitman.com>
2020-11-05 14:32:43 -0600

    PokemonList: successfully rendered initial list of PokemonListItem.

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,13 +1,30 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 
+// Dependencies
+import axios from 'axios';
 
 // Assets
 import placeholder from "../img/placeholder-image.png";
+import pokeball from "../img/pokeball.png";
 
 
-const PokemonListItem = () => {
+const PokemonListItem = (props) => {
+const [img, setImg] = useState(pokeball);
+const [name, setName] = useState('Pokemon');
+const [number, setNumber] = useState(" ");
 
 
+  useEffect(() => {
+    const getPokemon = async () => {
+      const {data} = await axios.get(props.detailsUrl);
+
+      setNumber(data.id);
+      setName(data.name);
+      setImg(data.sprites.front_default)
+      // console.log(data.sprites.other.official-artwork.front_default);
+    };
+    getPokemon();
+  }, []);
 
 
 
@@ -16,11 +33,11 @@ const PokemonListItem = () => {
     <div className='pokemon-list-item'>
       <img
         className='pokemon-list-item-img'
-        src={placeholder}
+        src={img}
         alt='pokemon art'
       />
-      <h4 className='pokemon-list-item-number'>7</h4>
-      <h4 className='pokemon-list-item-name'>Squirtle</h4>
+      <h4 className='pokemon-list-item-number'>{number}</h4>
+      <h4 className='pokemon-list-item-name'>{name}</h4>
     </div>
   );
 };
