1e1db15f79f2e7386c9e517daf5bea08bfcddcad
Casey The Great <casey@caseypitman.com>
2020-11-08 10:36:36 -0600

    Added close modal function to Display component.

--- src/components/Display.js
+++ src/components/Display.js
@@ -18,6 +18,10 @@ const Display = props => {
     setModalStatus(true);
   };
 
+  const closeModal = () => {
+    setModalStatus(false);
+  };
+
   return (
     <div className='display'>
       <PokemonList
@@ -25,7 +29,28 @@ const Display = props => {
         makeModal={makeModal}
       />
 
-      <ReactModal isOpen={modalStatus} overlayClassName='modal-overlay' className = 'modal-content-container'>
+      <ReactModal
+        isOpen={modalStatus}
+        overlayClassName='modal-overlay'
+        className='modal-content-container'
+        shouldFocusAfterRender={
+          true
+        }
+        onRequestClose={
+          closeModal
+        }
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={
+          true
+          /* Boolean indicating if pressing the esc key should close the modal
+           Note: By disabling the esc key from closing the modal
+           you may introduce an accessibility issue. */
+        }
+        shouldReturnFocusAfterClose={
+          true
+          /* Boolean indicating if the modal should restore focus to the element
+           that had focus prior to its display. */
+        }>
         <PokemonDetail />
       </ReactModal>
 
