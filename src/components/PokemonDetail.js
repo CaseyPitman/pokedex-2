d88e6a6a59cc0e25f9ab4a0c563c0a20677c0b18
Casey The Great <casey@caseypitman.com>
2020-11-08 19:25:02 -0600

    Added close modal button to PokemonDetail.

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -10,28 +10,33 @@ import EvolutionChain from "./EvolutionChain";
 // Styles
 import display from "../CSS/display.css";
 
-
-
 const PokemonDetail = () => {
   return (
     <div className='pokemon-detail'>
+      <i class='fas fa-times-circle close-modal-button'></i>
 
-      <img className = 'pokemon-detail-img' src={placeholder} alt='placeholder pic'  />
-
-
+      <Button type='left active' />
       <div className='pokemon-detail-content'>
-      <i class="fas fa-times-circle"></i>
-        <Button type='left active' />
-        <div className = 'pokemon-detail-content-text'>
-          <h2 className = 'pokemon-detail-content-text-number pokemon-detail-content-text-item'>7</h2>
-          <h2 className = 'pokemon-detail-content-text-name pokemon-detail-content-text-item'>Squirtle</h2>
+        <img
+          className='pokemon-detail-img'
+          src={placeholder}
+          alt='placeholder pic'
+        />
+        <div className='pokemon-detail-content-text'>
+          <h2 className='pokemon-detail-content-text-number pokemon-detail-content-text-item'>
+            7
+          </h2>
+          <h2 className='pokemon-detail-content-text-name pokemon-detail-content-text-item'>
+            Squirtle
+          </h2>
           {/* make type clickable */}
-          <h3 className = 'pokemon-detail-content-text-type pokemon-detail-content-text-item'>Water Type</h3>
-           <EvolutionChain />
+          <h3 className='pokemon-detail-content-text-type pokemon-detail-content-text-item'>
+            Water Type
+          </h3>
+          <EvolutionChain />
         </div>
-        <Button type='right active' />
       </div>
-     
+      <Button type='right active' />
     </div>
   );
 };
