4970d6c86c91d86bdf0d3e2c4876fe51a360d12e
Casey The Great <casey@caseypitman.com>
2020-11-09 19:19:50 -0600

    PokemonDetail: type list css tweaks (increased width)

--- src/components/PokemonTypeListItem.js
+++ src/components/PokemonTypeListItem.js
@@ -4,7 +4,7 @@ const PokemonTypeListItem = props => {
   return (
     <li className='pokemon-detail-content-type-list-item'>{`${
       props.name.charAt(0).toUpperCase() + props.name.slice(1)
-    } type`}</li>
+    } Type`}</li>
   );
 };
 
