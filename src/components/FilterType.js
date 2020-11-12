d862a9ab9b90dbe4aa32e49d4d80278f0f328763
Casey The Great <casey@caseypitman.com>
2020-11-12 11:37:59 -0600

    FIX: Search Bar - importing Search from wrong module.

--- src/components/FilterType.js
+++ src/components/FilterType.js
@@ -6,26 +6,27 @@ import header from "../CSS/header.css";
 
 const FilterType = ({ changeListType }) => {
   const [types, setTypes] = useState([]);
-  const [typeUrl, setTypeUrl] = useState('');
+  const [typeUrl, setTypeUrl] = useState("");
 
   // //Make call for list of types.
   useEffect(() => {
     const getTypes = async () => {
       try {
         const response = await axios.get("https://pokeapi.co/api/v2/type");
-        console.log('types')
-        console.log(response.data);
+        console.log("types");
+        // console.log(response.data);
         setTypes(response.data.results);
       } catch (error) {
         console.log("Something went wrong: ", error.message);
       }
     };
+
     getTypes();
   }, []);
 
   //Handles change in selector.
   const onChangeSelect = event => {
-    console.log(`selected type: ${event.target.value}`)
+    console.log(`selected type: ${event.target.value}`);
     changeListType(event.target.value);
   };
 
@@ -45,8 +46,12 @@ const FilterType = ({ changeListType }) => {
   return (
     <div className='filter-type'>
       <select className='input-field' onChange={onChangeSelect}>
-        <option key = 'default' value = 'all'>Sort by Type</option>
-        <option key = 'all' value = 'all'>All Pokémon</option>
+        <option key='default' value='all'>
+          Sort by Type
+        </option>
+        <option key='all' value='all'>
+          All Pokémon
+        </option>
 
         {options}
       </select>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Components
-import Search from "./FilterType"
+import Search from "./Search"
 import FilterType from "./FilterType";
 
 const SearchBar = (props) => {
