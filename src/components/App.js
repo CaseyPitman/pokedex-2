84fe6e45f15de181df53d017da67eb34534507e1
Casey The Great <casey@caseypitman.com>
2020-11-13 10:40:35 -0600

    App: added error handling to getData call.

--- src/components/App.js
+++ src/components/App.js
@@ -11,21 +11,21 @@ const App = () => {
   //Store list of all Pokemon to be rendered
   const [pokemonData, setPokemonData] = useState([]);
 
-  //  Call to retrieve list of all Pokemon and then construct the customized data object.
-
+  //  Call to retrieve list of all Pokemon and then store name and url.
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1050");
+        const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1050");
 
-        console.log(`init call:`);
-        console.log(response.data.results);
+        setPokemonData(data.results);
+
       } catch (error) {
-        console.log(error);
+        console.log(`Something has gone wrong: ${error}`);
       }
     };
     getData();
-  });
+  }, []);
+
 
   return (
     <div>
