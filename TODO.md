1c43e33dd42e88fa0d3152f4ee93c1130ec7f510
Casey The Great <casey@caseypitman.com>
2020-11-20 15:32:42 -0600

    SearchError: Mobile adjustments to width, spacing, and font sizes.

--- TODO.md
+++ TODO.md
@@ -5,18 +5,14 @@
 - tablet styles 
    -adjust search error page
 
-- mobile styles
-   - re-style grid to three wide
-   - stack filter & search
-   - list text size adjustment
-   - adjust display height
 
-
-   - landscape - adjust width/height detail modal
+   - mobile landscape - adjust width/height detail modal
    
 
+-enable search from searchError page.
 
 
+-land on search error page - execute scroll.
 
 
 
--- src/components/SearchError.js
+++ src/components/SearchError.js
@@ -20,7 +20,7 @@ const SearchError = ({changeDisplay}) => {
    return (
 
       <div className = 'search-error'>
-         <h1 className = 'search-error-headline'>Oh no! Something is wrong.</h1>
+         <h1 className = 'search-error-headline'>Oh no!<br/>Something is wrong.</h1>
          <img className = 'search-error-image' src = {searchErrorImage} alt = 'Sad Cubone is sad.'/>
          <h3 className = 'search-error-explanation'>The Pokémon you are searching for isn't here.<br/>Perhaps it does not exist.<br/>Perhaps it has not yet been catalogued.</h3>
          <h2 className = 'search-error-return-home' onClick = {onClick}>[HOME]</h2>
