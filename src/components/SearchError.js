3f0824a8d8ddc6086be7f9390a0f60c282840111
Casey The Great <casey@caseypitman.com>
2020-11-21 17:19:17 -0600

    CSS clean up.

--- src/components/SearchError.js
+++ src/components/SearchError.js
@@ -1,27 +1,40 @@
-
 //This module displays when there is an error in the search (user searches for non-existent pokemon);
 
-import React from 'react';
+import React from "react";
 
 // Assets
-import searchErrorImage from '../img/sad-cubone.png'
+import searchErrorImage from "../img/sad-cubone.png";
 
-const SearchError = ({changeDisplay}) => {
+const SearchError = ({ changeDisplay }) => {
+  //Use clicks home to return to normal view.
+  const onClick = () => {
+    changeDisplay("normal");
+  };
 
-   //Use clicks home to return to normal view.
-   const onClick = () => {
-      changeDisplay('normal');
-   }
-
-   return (
-
-      <div className = 'search-error'>
-         <h1 className = 'search-error-headline'>Oh no!<br/>Something is wrong.</h1>
-         <img className = 'search-error-image' src = {searchErrorImage} alt = 'Sad Cubone is sad.'/>
-         <h3 className = 'search-error-explanation'>The Pokémon you are searching for isn't here.<br/>Perhaps it does not exist.<br/>Perhaps it has not yet been catalogued.</h3>
-         <h2 className = 'search-error-return-home' onClick = {onClick}>[HOME]</h2>
-      </div>
-   )
-}
+  return (
+    <div className='search-error'>
+      <h1 className='search-error-headline'>
+        Oh no!
+        <br />
+        Something is wrong.
+      </h1>
+      <img
+        className='search-error-image'
+        src={searchErrorImage}
+        alt='Sad Cubone is sad.'
+      />
+      <h3 className='search-error-explanation'>
+        The Pokémon you are searching for isn't here.
+        <br />
+        Perhaps it does not exist.
+        <br />
+        Perhaps it has not yet been catalogued.
+      </h3>
+      <h2 className='search-error-return-home' onClick={onClick}>
+        [HOME]
+      </h2>
+    </div>
+  );
+};
 
 export default SearchError;
