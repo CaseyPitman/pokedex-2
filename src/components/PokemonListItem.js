f9aecef2204570a2e5ead81c9c6b3bb2b7fbd59d
Casey The Great <casey@caseypitman.com>
2020-11-21 16:57:48 -0600

    PokemonListItem: clean up comments and console.logs.

--- src/components/PokemonListItem.js
+++ src/components/PokemonListItem.js
@@ -1,17 +1,20 @@
+// This component renders each pokemon to be displayed in currently selected list.
+
 import React, { useState, useEffect } from "react";
 
 // Dependencies
 import axios from "axios";
 
-// Assets
+// Assets (laoding image / placeholder image for pokemon with no image)
 import pokeball from "../img/pokeball.png";
 
 const PokemonListItem = props => {
+  // Store the info to be dsplayed (set to defaults)
   const [img, setImg] = useState(pokeball);
-  const [name, setName] = useState("???");
-  const [number, setNumber] = useState("?");
+  const [name, setName] = useState("Name?");
+  const [number, setNumber] = useState("0");
 
-  // Calls for Pokemon image, number, and name to display on list.
+  // Call for Pokemon image, number, and name to display on list.
   useEffect(() => {
     const source = axios.CancelToken.source();
 
@@ -20,20 +23,24 @@ const PokemonListItem = props => {
         const { data } = await axios.get(props.detailsUrl, {
           cancelToken: source.token,
         });
-
+        // Set pokemon number
         setNumber(data.id);
-
+        //If there is no available sprite image, use the pokeball placeholder
         if (data.sprites.front_default === null) {
           setImg(pokeball);
-        } else {
+        }
+        // If there is an available sprite, then store and display it.
+        else {
           setImg(data.sprites.front_default);
         }
+        // Set pokemon name
         setName(data.name);
       } catch (error) {
         if (axios.isCancel(error)) {
           //User cancels request
           console.log("Request canceled", error.message);
         } else {
+          // Some other error.
           console.log("Something went wrong: ", error.message);
         }
       }
@@ -45,9 +52,9 @@ const PokemonListItem = props => {
     };
   }, [props.detailsUrl]);
 
+  // User clicks a pokmeon to request detailed view
   const handleClick = () => {
-    // pass the detail url and the index from current list of pokemon displayed.
-
+    // Pass the detail url and the index from current list of pokemon displayed.
     props.makeModal(props.index);
   };
 
@@ -59,8 +66,8 @@ const PokemonListItem = props => {
         alt={`Classic ${name} sprite.`}
       />
       <h4 className='pokemon-list-item-number'>{number}</h4>
-      {/* Capitalize Pokemon name */}
       <h4 className='pokemon-list-item-name'>
+        {/* Capitalize Pokemon name */}
         {name.charAt(0).toUpperCase() + name.slice(1)}
       </h4>
     </div>
