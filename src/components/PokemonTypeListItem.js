efef18b251ca6d2c5dd81524bcda4b1cd5fefc8a
Casey The Great <casey@caseypitman.com>
2020-11-21 17:02:43 -0600

    Pokmeon TypeListItem: clean up comments and changed color of type list items.

--- src/components/PokemonTypeListItem.js
+++ src/components/PokemonTypeListItem.js
@@ -1,20 +1,17 @@
+// This component renders the types of the pokmeon types in the PokemonDetail component
+
 import React from "react";
 
-
-
-
-
 const PokemonTypeListItem = props => {
-
-const onTypeClick = () => {
-  props.changeListType(props.name)
-}
-
+  // User clicks on one of the types - will close modal and display pokemon of that type.
+  const onTypeClick = () => {
+    props.changeListType(props.name);
+  };
 
   return (
-    <li className='pokemon-detail-content-type-list-item' onClick = {onTypeClick}>{`${
-      props.name.charAt(0).toUpperCase() + props.name.slice(1)
-    } Type`} </li>
+    <li className='pokemon-detail-content-type-list-item' onClick={onTypeClick}>
+      {`${props.name.charAt(0).toUpperCase() + props.name.slice(1)} Type`}{" "}
+    </li>
   );
 };
 
